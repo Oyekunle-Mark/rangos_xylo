dcbc13208e9cd4ffaa06ae93961d8f555ed8ba2b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 08:41:18 +0100

    Wrapped with a column

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,16 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Play'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Play'),
+              ),
+            ],
           ),
         ),
       ),
